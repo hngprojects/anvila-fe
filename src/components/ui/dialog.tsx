83fde203748f83b5a42fd1f2eb441c7ec121eeb8
Rayflix55 <rayflix55@gmail.com>
2026-05-13 14:30:33 +0100

    chore: migrate to pnpm and fix build script permissions

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { cn } from '../../utils'
-import { Cross2Icon } from '../../../node_modules/@radix-ui/react-icons'
+import { Cross2Icon } from '@radix-ui/react-icons'
 
 const Dialog = DialogPrimitive.Root
 
